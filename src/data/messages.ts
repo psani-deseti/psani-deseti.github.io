4e86b478c64dacaf001f91a3f3d09c30d0fcff0f
Diamond <112625718+diamond-dev1@users.noreply.github.com>
2026-03-20 15:56:48 +0100

    text

--- src/data/messages.ts
+++ src/data/messages.ts
@@ -1,23 +1,35 @@
 export const perfectMessages = [
-  "Jsi nejlepší!",
-  "Fantastické!",
+  "Jedeš jako sigma",
+  "No nekecej, takle dobrý?",
   "Mistrovský výkon!",
-  "Bezchybné!",
-  "Jsi pinba!"
+  "To čumim",
+  "Jsi fakt pinba!",
+  "Pinba je nejlepší!"
 ];
 
 export const goodMessages = [
   "Skvělá práce!",
-  "Jde ti to!",
+  "Pinba je nejlepší!"
+  "Jde ti to, ale ještě trochu mákni.",
   "Jen tak dál!",
   "Super!",
   "Výborně!"
+  "Makej makej!",
+  "Pinba je nejlepší!",
+  "Zlepšuješ se",
+  "Ahoj, tohle je fakt super tajná zpráva",
+  "Má rychlost 6 až 7 slov!",
+  "pinbitko jede",
+  "Nemluv u toho, Roblox už čeká"
 ];
 
 export const tryAgainMessages = [
   "Nevzdávej to!",
-  "Pomalu to nevadí.",
+  "Klidně pomalu, to nevadí.",
   "Tos dopinbil...",
   "Trénink dělá mistra.",
-  "Chybami se člověk učí."
+  "Chybami se člověk učí.",
+  "Žádný učený z nebe nespadl...",
+  "Trénuj, trénuj, ať jsi nejlepší!",
+  "Však ono to půjde"
 ];
